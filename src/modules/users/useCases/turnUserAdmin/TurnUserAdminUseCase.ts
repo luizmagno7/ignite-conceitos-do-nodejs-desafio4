fca356af506158dbda48c835110b7fe8d0173f44
Luiz Magno <luiz.magno7@yahoo.com.br>
2021-11-01 01:38:23 -0300

    finalizado

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -5,11 +5,24 @@ interface IRequest {
   user_id: string;
 }
 
+function UserException(message: string, status: number) {
+  this.message = message;
+  this.status = status;
+}
+
 class TurnUserAdminUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new UserException("User doesn't exists!", 404);
+    }
+
+    this.usersRepository.turnAdmin(user);
+
+    return user;
   }
 }
 
